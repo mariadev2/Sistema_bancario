47d107b696149499c16a5ec0717cb913008f163d
Maria Eduarda <101299266+mariadev2@users.noreply.github.com>
2023-06-20 13:02:25 -0300

    iniciando desafio

---
+++ sistema_bancario.py
@@ -0,0 +1,23 @@
+class Cliente:
+    def __init__ (self,nome,endereco):
+        self.nome= nome
+        self.endereco = endereco
+        self.conta = 0
+
+    def criar_conta(self):
+        self.conta += 1
+        self.nome= input("Digite seu nome completo: \n")
+        self.endereco= input("Digite seu endereço completo: \n")
+        print("conta criada com sucesso!")
+
+#    def __str__ (self):
+#        return f"{self.__class__.__name__}:{', '.join([f'{chave}={valor}'for chave, valor in self.__dict__.items()])}"
+while True:
+    opcao= int(input("opcao:\n"))
+    if opcao == 1:
+        cliente = Cliente("","")
+        cliente.criar_conta()
+        print(f'{cliente.nome},{cliente.endereco},{cliente.conta}')
+        
+    else:
+        break
